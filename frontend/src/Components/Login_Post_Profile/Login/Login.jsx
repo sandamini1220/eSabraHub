20c0352ca9aa1f8e61634a45223e7e3518f58093
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2024-07-04 20:36:23 +0530

    Name
    ...

---
+++ frontend/src/Components/Login_Post_Profile/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Login.css'
+
+export default function Login() {
+  return (
+    <div className='page'>
+      <h1 className='Head-Name'>Welcome to eSabraHub!!!</h1>
+    </div>
+  )
+}
