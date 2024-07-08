7a41537d74986f162044672842ea13364c35c40c
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2024-07-08 11:57:26 +0530

    why us part of about us

--- frontend/src/Components/HomePageCompo/AboutUs/WhyUs/WhyUs.jsx
+++ frontend/src/Components/HomePageCompo/AboutUs/WhyUs/WhyUs.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './WhyUs.css'
+import girl from '../../../../Assets/whyus-team.jpg'
+import tick from '../../../../Assets/tick.png'
+
 const WhyUs = () => {
   return (
-    <div className='whyus-aboutus'>
-      qhy us
+    <div className='aboutus-whyus-container'>
+      <div className="aboutus-whyus-image">
+      <img src={girl} alt="" />
+      </div>
+      <div className="aboutus-whyus-text">
+        <div className="aboutus-whyus-text-topic">
+          <h1>WHY</h1><span>US</span>
+        </div>
+        <p>We provide accurate local service information and a user-friendly 
+          platform for a seamless experience. Stay connected through real-time 
+          chat in a trusted community of verified users.Our dedicated team is always ready to 
+          assist with any questions or concerns.</p>
+
+          <div className="list-items-aboutus-whyus">
+            <img src={tick} alt="" />
+            <p>Comprehensive Information</p>
+          </div>
+
+          <div className="list-items-aboutus-whyus">
+            <img src={tick} alt="" />
+            <p>User-Friendly Platform</p>
+          </div>
+
+          <div className="list-items-aboutus-whyus">
+            <img src={tick} alt="" />
+            <p>Real-Time Communication</p>
+          </div> 
+          
+          <div className="list-items-aboutus-whyus">
+            <img src={tick} alt="" />
+            <p>Trusted Community</p>
+          </div> 
+          
+          <div className="list-items-aboutus-whyus">
+            <img src={tick} alt="" />
+            <p>Continuous Support</p>
+          </div>
+
+      </div>
+      
     </div>
   )
 }
