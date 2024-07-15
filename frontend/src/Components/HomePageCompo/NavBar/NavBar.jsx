7b5fdb30197a20755d513da9870f7216659b0123
Prabodha Harshani <prabodaharshani95@gmail.com>
2024-07-15 14:50:24 +0530

    responsive navbar

--- frontend/src/Components/HomePageCompo/NavBar/NavBar.jsx
+++ frontend/src/Components/HomePageCompo/NavBar/NavBar.jsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import {  Link  } from 'react-scroll';
+import { Link } from 'react-scroll';
 import './NavBar.css';
 import logo from '../../../Assets/logo-eSabraHub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faNavicon } from '@fortawesome/free-solid-svg-icons';
+import { faNavicon, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [dropdown, setDropdown] = useState(false);
   const [targetSection, setTargetSection] = useState(null);
+  const [sideNavOpen, setSideNavOpen] = useState(false);
 
   const handleMouseEnter = () => {
     setDropdown(true);
@@ -26,6 +27,7 @@ const NavBar = () => {
 
   const handleServiceClick = (servicePath) => {
     navigate(servicePath);
+    setSideNavOpen(false); // Close side navbar on link click
   };
 
   const handleScrollToSection = (sectionId) => {
@@ -35,6 +37,7 @@ const NavBar = () => {
     } else {
       scrollToSection(sectionId);
     }
+    setSideNavOpen(false); // Close side navbar on link click
   };
 
   const scrollToSection = (sectionId) => {
@@ -52,7 +55,6 @@ const NavBar = () => {
     if (targetSection) {
       const sectionElement = document.getElementById(targetSection);
       if (sectionElement) {
-        // Calculate the top position relative to the viewport
         const top = sectionElement.offsetTop + window.pageYOffset - 100;
         window.scrollTo({
           top,
@@ -63,13 +65,17 @@ const NavBar = () => {
     }
   }, [targetSection, location.pathname]);
 
+  const toggleSideNav = () => {
+    setSideNavOpen(!sideNavOpen);
+  };
+
   return (
     <div className="navibar">
       <nav>
         <div className="logo" onClick={() => navigate('/')}>
           <img src={logo} alt="eSabraHub Logo" />
         </div>
-        <ul>
+        <ul className='display-none-navibar'>
           <li className="navlist">
             <Link
               to="heroId"
@@ -138,11 +144,28 @@ const NavBar = () => {
           <li>
             <button onClick={handleLoginClick}>Login</button>
           </li>
+          
+        </ul>
+     
+       <FontAwesomeIcon icon={faNavicon} className="navbar-icon" onClick={toggleSideNav} />
+     
+      </nav>
+      <div className={`side-navbar ${sideNavOpen ? 'open' : ''}`}>
+        <FontAwesomeIcon icon={faTimes} className="close-icon" onClick={toggleSideNav} />
+        <ul >
+          <li onClick={() => handleScrollToSection('heroId')}>Home</li>
+          <li onClick={() => handleServiceClick('/accommodation')}>Accommodations</li>
+          <li onClick={() => handleServiceClick('/transport')}>Transport</li>
+          <li onClick={() => handleServiceClick('/food')}>Food Shops</li>
+          <li onClick={() => handleServiceClick('/medicine')}>Medical Center</li>
+          <li onClick={() => handleServiceClick('/places')}>Attractive Places</li>
+          <li onClick={() => handleScrollToSection('aboutId')}>About Us</li>
+          <li onClick={() => handleScrollToSection('contactId')}>Contact Us</li>
           <li>
-          <FontAwesomeIcon icon={faNavicon} className="contactus-icon"/>
+            <button onClick={handleLoginClick}>Login</button>
           </li>
         </ul>
-      </nav>
+      </div>
     </div>
   );
 };
