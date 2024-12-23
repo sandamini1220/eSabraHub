e7be38cc453b00872976ba2f9178ef4cc7a3b97b
mhmdfashan <mohammedfashan16@gmail.com>
2024-12-23 14:08:19 +0530

    ChatBot Feature Added

---
+++ frontend/src/Pages/Chatbot/Components/ChatMessage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Chatboticon from './Chatboticon'
+
+const ChatMessage = ({chat}) => {
+  return (
+    !chat.hideInChat && (
+    <div className={`message ${chat.role === "model"? 'bot':'user'}-message`}>
+        {chat.role == "model" && <Chatboticon/>}
+        <p className="message-text">{chat.text} </p>
+      
+    </div>
+    )
+  );
+}
+
+export default ChatMessage
