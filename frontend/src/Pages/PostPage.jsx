e35514f8e3003cd8e56fa1218e7f7a8d5bac56ea
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2024-12-26 12:13:38 +0530

    Update the Frontend
    Implemented post and profile pages with responsive design, ensuring smooth user interaction and enhanced aesthetics. Key features include optimized layouts and seamless integration with existing components.

--- frontend/src/Pages/PostPage.jsx
+++ frontend/src/Pages/PostPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import CreatePost from '../Components/PostsPageCompo/CreatePost/CreatePost'
+import CreatePostNavBar from '../Components/PostsPageCompo/CreatePostnavBar/CreatePostNavBar'
+import Posts from '../Components/PostsPageCompo/Posts/Posts'
 
 const PostPage = () => {
   return (
     <div>
-
+      <CreatePostNavBar/>
+      <Posts/>
     </div>
   )
 }
 
 export default PostPage
-
